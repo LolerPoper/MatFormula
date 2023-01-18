01d76a35fc9f97c2bf2249fd54241ca8067a22fb
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-01-18 08:24:28 +0100

    Add files via upload

---
+++ MatFormula/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatFormula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 23.5, b = 12.8, c = 67, d = 10.543;
+            double x = (2 * a + 2 * b + 2 * c + 2 * d) / (2 * (a + b) + 2 * (c + d));
+
+            Console.WriteLine(x);
+            
+            Console.ReadKey();
+        }
+    }
+}
